00276f4ae18ce6aff8f669a99f4852d7323f4134
ssfinka <safinkapplg@gmail.com>
2024-02-18 15:35:01 +0700

    menambahkan list

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Coba Git Remote</title>
+    </head>
+    <body>
+        <h1>Hello world!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio sunt animi repellendus porro suscipit fugiat quam nulla tenetur dolor ratione unde, eligendi, quaerat, facere expedita? Error iste enim in quod!</p>
+        <ul>
+            <li>satu</li>
+            <li>dua</li>
+        </ul>
+    </body>
+</html>
